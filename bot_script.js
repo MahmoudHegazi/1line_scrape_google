2d2a8a73bb8d5f2d10a2df91d81d4718899035c2
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2024-05-31 17:23:07 +0300

    Update bot_script.js

--- bot_script.js
+++ bot_script.js
@@ -2,7 +2,7 @@ console.log(Array.from(Array.from(document.querySelectorAll("#main div[data-hvei
     const urlElm = cont.querySelector("a[data-ved][jsname][href]:not([href=''])");
     if (urlElm){
         const url = urlElm && urlElm.href.trim() ? urlElm.href.trim() : '';
-        let description = cont.querySelector("div[data-sncf='1'] span");
+        let description = cont.querySelector("div[data-snc] div[style^='-webkit-line-clamp']");
         description = (description && description.innerText.trim()) ? description.innerText.trim() : '';
         let title = urlElm.querySelector("div.notranslate div[class] div:not([class]) span");
         title = (title && title.innerText.trim()) ? title.innerText.trim() : '';
