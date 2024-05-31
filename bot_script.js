91a5f8f072c33adaeed4710bcf551fd50961b2b2
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2024-05-31 17:05:57 +0300

    Create bot_script.js

---
+++ bot_script.js
@@ -0,0 +1,13 @@
+console.log(Array.from(Array.from(document.querySelectorAll("#main div[data-hveid][data-ved] div[data-async-context] div[jscontroller][lang] div[data-snc]")).map((cont)=>{
+    const urlElm = cont.querySelector("a[data-ved][jsname][href]:not([href=''])");
+    if (urlElm){
+        const url = urlElm && urlElm.href.trim() ? urlElm.href.trim() : '';
+        let description = cont.querySelector("div[data-sncf='1'] span");
+        description = (description && description.innerText.trim()) ? description.innerText.trim() : '';
+        let title = urlElm.querySelector("div.notranslate div[class] div:not([class]) span");
+        title = (title && title.innerText.trim()) ? title.innerText.trim() : '';
+        return (title && description && url) ? {title, description, url} : false;
+    } else {
+        return false;
+    }
+}).filter((obj)=>{return obj})));
